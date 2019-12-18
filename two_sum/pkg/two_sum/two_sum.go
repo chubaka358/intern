fcb81aa1250ae66f439168c12be11ad4ad84b025
Dmitry Krasnov <chubaka358@icloud.com>
2019-12-18 14:48:31 +0300

    Added README.md and comments to func

--- two_sum/pkg/two_sum/two_sum.go
+++ two_sum/pkg/two_sum/two_sum.go
@@ -1,15 +1,21 @@
 package two_sum
 
-
+// Function to return indices of the two numbers such that they add up to a specific target
 func TwoSum(nums []int, target int) []int {
+	// define map containing iterated integers from nums
 	numbersMap := make(map[int]int)
-	for i := 0; i < len(nums); i++{
+
+	// Loop that iterates over nums slice
+	for i := 0; i < len(nums); i++ {
+		// calculate complement number
 		complement := target - nums[i]
-		if val, ok := numbersMap[complement]; ok{
+		// check contains numbersMap complement or not. Return if contains
+		if val, ok := numbersMap[complement]; ok {
 			return []int{val, i}
 		}
+		// add iterated value in numbersMap
 		numbersMap[nums[i]] = i
 	}
+	// incorrect input
 	panic("Wrong arguments")
 }
-

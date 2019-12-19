0bdfeca81b8ffb04a6e08fdcf89018e2addfb292
Dmitry Krasnov <chubaka358@icloud.com>
2019-12-19 13:18:07 +0300

    reformat code

--- cmd/isomorphic-strings/main.go
+++ cmd/isomorphic-strings/main.go
@@ -3,7 +3,8 @@ package main
 import (
 	"fmt"
 
-	"github.com/chubaka358/intern/pkg/isomorphic-strings")
+	"github.com/chubaka358/intern/pkg/isomorphic-strings"
+)
 
 func main() {
 	fmt.Println(isomorphic_strings.IsIsomorphic("ab", "aa"))
--- pkg/isomorphic-strings/isomorphic_strings_test.go
+++ pkg/isomorphic-strings/isomorphic_strings_test.go
@@ -1,35 +1,36 @@
 package isomorphic_strings
 
 import (
-	"github.com/google/go-cmp/cmp"
 	"testing"
+
+	"github.com/google/go-cmp/cmp"
 )
 
 func TestIsomorphicStrings(t *testing.T) {
-	tests := map[string]struct{
+	tests := map[string]struct {
 		inputS string
 		inputT string
-		want bool
+		want   bool
 	}{
-		"simpleTest1" : {"egg", "add", true},
-		"simpleTest2" : {"foo", "bar", false},
-		"simpleTest3" : {"paper", "title", true},
-		"simpleTest4" : {"aa", "ab", false},
-		"simpleTest5" : {"ab", "aa", false},
-		"simpleTest6" : {"aab", "aaa", false},
-		"simpleTest7" : {"zz", "zz", true},
-		"ChineseTest" : {"汉字", "漢字", true},
-		"emojiTest" : {"😄😎😎", "🥏🥎🥎", true},
-		"differentLength" : {"dog", "doge", false},
-		"emptyStrings" : {"", "", true},
-		"singleIdenticalCharacter" : {"a", "a", true},
-		"singleNonidenticalCharacter" : {"a", "b", true},
+		"simpleTest1":                 {"egg", "add", true},
+		"simpleTest2":                 {"foo", "bar", false},
+		"simpleTest3":                 {"paper", "title", true},
+		"simpleTest4":                 {"aa", "ab", false},
+		"simpleTest5":                 {"ab", "aa", false},
+		"simpleTest6":                 {"aab", "aaa", false},
+		"simpleTest7":                 {"zz", "zz", true},
+		"ChineseTest":                 {"汉字", "漢字", true},
+		"emojiTest":                   {"😄😎😎", "🥏🥎🥎", true},
+		"differentLength":             {"dog", "doge", false},
+		"emptyStrings":                {"", "", true},
+		"singleIdenticalCharacter":    {"a", "a", true},
+		"singleNonidenticalCharacter": {"a", "b", true},
 	}
-	for name, tc := range tests{
+	for name, tc := range tests {
 		t.Run(name, func(t *testing.T) {
 			got := IsIsomorphic(tc.inputS, tc.inputT)
 			diff := cmp.Diff(tc.want, got)
-			if diff != ""{
+			if diff != "" {
 				t.Fatalf(diff)
 			}
 		})

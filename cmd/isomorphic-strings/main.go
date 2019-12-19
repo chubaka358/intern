111e7bc6aa8352f6b83b440da4ada82422292b4f
Dmitry Krasnov <chubaka358@icloud.com>
2019-12-19 12:24:01 +0300

    change import path

--- cmd/isomorphic-strings/main.go
+++ cmd/isomorphic-strings/main.go
@@ -3,7 +3,7 @@ package main
 import (
 	"fmt"
 
-	"intern/pkg/isomorphic-strings")
+	"github.com/chubaka358/intern/pkg/isomorphic-strings")
 
 func main() {
 	fmt.Println(isomorphic_strings.IsIsomorphic("ab", "aa"))

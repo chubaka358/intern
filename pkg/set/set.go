aff122da377a4795f2cc83a8a127fae81ad857da
Dmitry Krasnov <chubaka358@icloud.com>
2019-12-20 11:07:09 +0300

    change value semantic to pointer semantic

--- pkg/set/set.go
+++ pkg/set/set.go
@@ -19,37 +19,37 @@ func NewSet() set {
 }
 
 // Add adds s to the set
-func (set set) Add(s ...string) {
-	for _, element := range s {
-		set[element] = true
+func (s *set) Add(elements ...string) {
+	for _, element := range elements {
+		(*s)[element] = true
 	}
 }
 
 // Delete removes s from a set
-func (set set) Delete(s string) {
-	delete(set, s)
+func (s *set) Delete(element string) {
+	delete(*s, element)
 }
 
 // Size returns the number of elements in the set
-func (set set) Size() int {
-	return len(set)
+func (s *set) Size() int {
+	return len(*s)
 }
 
 // Contains checks if the set contains s element or not
 // Returns true if contains, else false
-func (set set) Contains(s string) bool {
-	_, ok := set[s]
+func (s *set) Contains(element string) bool {
+	_, ok := (*s)[element]
 	return ok
 }
 
 // Intersection returns a new set that is the intersection of set and anotherSet
-func (set set) Intersection(anotherSet set) set {
+func (s *set) Intersection(anotherS *set) set {
 	intersect := make(map[string]int)
 	interSet := NewSet()
-	for key := range set {
+	for key := range *s {
 		intersect[key]++
 	}
-	for key := range anotherSet {
+	for key := range *anotherS {
 		intersect[key]++
 	}
 	for key, val := range intersect {
@@ -61,14 +61,14 @@ func (set set) Intersection(anotherSet set) set {
 }
 
 // Union returns a new set, which is the union of set and anotherSet
-func (set set) Union(anotherSet set) set {
+func (s *set) Union(anotherS *set) set {
 	unionSet := NewSet()
-	for key := range set {
+	for key := range *s {
 		if !unionSet.Contains(key) {
 			unionSet.Add(key)
 		}
 	}
-	for key := range anotherSet {
+	for key := range *anotherS {
 		if !unionSet.Contains(key) {
 			unionSet.Add(key)
 		}

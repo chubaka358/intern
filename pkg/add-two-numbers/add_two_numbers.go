96d8b043ad0e8cb566107cb3f6c82ce5c1d3d30c
Dmitry Krasnov <chubaka358@icloud.com>
2020-01-13 21:59:27 +0300

    add_two_numbers impl

---
+++ pkg/add-two-numbers/add_two_numbers.go
@@ -0,0 +1,72 @@
+package add_two_numbers
+
+// ListNoder provides listNode interface
+type ListNoder interface {
+	AddTwoNumbers(l1 *listNode, l2 *listNode) *listNode
+	SetNextNode(list *listNode) *listNode
+	GetValue() int
+	GetNext() *listNode
+}
+
+// listNode implements list node
+type listNode struct {
+	val  int
+	next *listNode
+}
+
+// GetValue returns listNode value
+func (l *listNode) GetValue() int {
+	return l.val
+}
+
+// GetNext returns listNode next
+func (l *listNode) GetNext() *listNode {
+	return l.next
+}
+
+// AddTwoNumbers returns the result of adding two numbers
+func (l *listNode) AddTwoNumbers(l1 *listNode, l2 *listNode) *listNode {
+	head := &listNode{}
+	p := l1
+	q := l2
+	curr := head
+	carry := 0
+	for p != nil || q != nil {
+		sum := 0
+		if p != nil {
+			sum += p.val
+		}
+		if q != nil {
+			sum += q.val
+		}
+		sum += carry
+		carry = sum / 10
+		curr.next = &listNode{
+			val: sum % 10,
+		}
+		curr = curr.next
+		if p != nil {
+			p = p.next
+		}
+		if q != nil {
+			q = q.next
+		}
+	}
+	if carry > 0 {
+		curr.next = &listNode{
+			val: 1,
+		}
+	}
+	return head.next
+}
+
+// NextNode sets next node for listNode
+func (l *listNode) SetNextNode(list *listNode) *listNode {
+	l.next = list
+	return l.next
+}
+
+// NewListNode returns new listNode
+func NewListNode(val int) *listNode {
+	return &listNode{val, nil}
+}

2adfd688e52ae62ae9058504bb29d1da0ca76ace
Dmitry Krasnov <chubaka358@icloud.com>
2020-01-13 14:54:37 +0300

    add comments

--- pkg/reverse-linked-list/reverse_list.go
+++ pkg/reverse-linked-list/reverse_list.go
@@ -1,5 +1,6 @@
 package reverse_linked_list
 
+// ListNoder provides listNode interface
 type ListNoder interface {
 	ReverseList() *listNode
 	SetNextNode(val int) *listNode
@@ -26,19 +27,23 @@ func (l *listNode) ReverseList() *listNode {
 	return prev
 }
 
+// GetValue returns listNode val
 func (l *listNode) GetValue() int {
 	return l.val
 }
 
+// GetNext returns listNode next
 func (l *listNode) GetNext() *listNode {
 	return l.next
 }
 
+// SetNextNode sets next node for listNode and returns pointer to next node
 func (l *listNode) SetNextNode(val int) *listNode {
 	l.next = NewListNode(val)
 	return l.next
 }
 
+// NewListNode returns new listNode
 func NewListNode(val int) *listNode {
 	return &listNode{val: val}
 }

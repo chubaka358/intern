9b8539fbb2b53b0e55ee35723080a0af9181c316
Dmitry Krasnov <chubaka358@icloud.com>
2020-01-15 14:20:14 +0300

    изменил main, чтобы он использовал отдельный merger

--- cmd/merge-trees/main.go
+++ cmd/merge-trees/main.go
@@ -7,10 +7,11 @@ import (
 )
 
 func main() {
+	merger := merge_trees.NewMerge()
 	tree1 := merge_trees.NewTreeNode()
 	tree1.Val = 1
 	tree2 := merge_trees.NewTreeNode()
 	tree2.Val = 5
 
-	fmt.Println(tree1.MergeTrees(tree1, tree2))
+	fmt.Println(merger.MergeTrees(tree1, tree2))
 }
